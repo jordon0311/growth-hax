bd3c794d334f9e379d8ac7745fc5d1370e492351
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-07-13 17:16:42 -0400

    fetch

--- devpagesFE/src/App.tsx
+++ devpagesFE/src/App.tsx
@@ -2,18 +2,34 @@ import Navbar from './components/Navbar'
 import AboutPage from './Pages/AboutPage'
 import ProjectsPage from './Pages/ProjectsPage'
 import ContactsPage from './Pages/ContactsPage'
-import { returnedData } from './sampleData'
+// import { returnedData } from './sampleData'
 import { ReturnedDataType } from './types.types'
+import { useState, useEffect } from 'react'
 
 function App() {
-
-  const data = returnedData as ReturnedDataType
+  const [data, setData] = useState<ReturnedDataType | null>(null)
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log('HELLLOOO fetching data')
+      try {
+        const response = await fetch('http://localhost:3000/user/jakezegil/dev_page', { mode: 'cors' })
+        const data = await response.json()
+        console.log('receieved', data)
+        setData(data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+    fetchData()
+  }, [])
+  console.log('bruhhh')
+  console.log('hereee', data)
   return (
     <>
-      <Navbar data={data} />
+      {/* <Navbar data={data} />
       <AboutPage data={data} />
       <ProjectsPage data={data} />
-      <ContactsPage data={data} />
+      <ContactsPage data={data} /> */}
     </>
   )
 }
