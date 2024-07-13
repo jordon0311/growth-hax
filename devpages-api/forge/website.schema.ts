4cb001f55e6bfb47c025d37f03cc1036b0a67a7d
Jake Zegil <jakezegil@gmail.com>
2024-07-13 15:31:11 -0400

    add some descriptions to the schema to help with parsing

--- devpages-api/forge/website.schema.ts
+++ devpages-api/forge/website.schema.ts
@@ -3,21 +3,33 @@ import z from 'zod';
 
 // Define the main person schema
 const WebsiteSchema = z.object({
-  bio: z.string().max(500).min(300),
+  bio: z
+    .string()
+    .max(500)
+    .min(300)
+    .describe('A short bio of the user, between 300 and 500 characters long'),
   colorPalette: z.object({
     accent: z.string().regex(/^#[0-9A-F]{6}$/i),
     background: z.string().regex(/^#[0-9A-F]{6}$/i),
     foreground: z.string().regex(/^#[0-9A-F]{6}$/i),
     primary: z.string().regex(/^#[0-9A-F]{6}$/i),
   }),
-  fontFamily: z.string(),
-  interests: z.array(z.string()).min(5),
+  fontFamily: z
+    .string()
+    .describe('The font family for the website (formatted for css)'),
+  interests: z
+    .array(z.string())
+    .min(5)
+    .describe('A list of interests, at least 5 long'),
   location: z.object({
     city: z.string(),
     state: z.string(),
   }),
-  quote: z.string(),
-  skills: z.array(z.string()).min(6),
+  quote: z.string().describe('A very profound quote from the user'),
+  skills: z
+    .array(z.string())
+    .min(6)
+    .describe('A list of skills, at least 6 long'),
 });
 
 export default WebsiteSchema;
